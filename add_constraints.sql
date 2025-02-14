157ce0dcafbe2e0252d1210401e57d4052e1986d
Sai Kumar Reddy Katika <saikumar.k@bridgehorn.com>
2025-02-14 16:19:47 +0530

    Initial commit

--- add_constraints.sql
+++ add_constraints.sql
@@ -1,5 +1,15 @@
--- Check primary keys
-SHOW PRIMARY KEYS IN SCHEMA cortex_analyst_demo.healthcare;
+-- Add foreign key constraints
+ALTER TABLE cortex_analyst_demo.healthcare.appointments
+ADD CONSTRAINT pid_fk_app
+FOREIGN KEY (patient_id)
+REFERENCES cortex_analyst_demo.healthcare.patients (patient_id);
 
--- Check foreign keys
-SHOW IMPORTED KEYS IN SCHEMA cortex_analyst_demo.healthcare;
+ALTER TABLE cortex_analyst_demo.healthcare.appointments
+ADD CONSTRAINT provid_fk_app
+FOREIGN KEY (provider_id)
+REFERENCES cortex_analyst_demo.healthcare.providers (provider_id);
+
+ALTER TABLE cortex_analyst_demo.healthcare.schedule_templates
+ADD CONSTRAINT provid_fk_sch
+FOREIGN KEY (provider_id)
+REFERENCES cortex_analyst_demo.healthcare.providers (provider_id);
---
+++ load_data.sql
@@ -0,0 +1,99 @@
+USE ROLE cortex_user_role;
+USE DATABASE cortex_analyst_demo;
+USE SCHEMA cortex_analyst_demo.healthcare;
+USE WAREHOUSE cortex_analyst_wh;
+
+COPY INTO cortex_analyst_demo.healthcare.appointments
+FROM @raw_data
+FILES = ('appointments.csv.gz')
+FILE_FORMAT = (
+    TYPE=CSV,
+    COMPRESSION=GZIP,
+    SKIP_HEADER=1,
+    FIELD_DELIMITER=',',
+    TRIM_SPACE=FALSE,
+    FIELD_OPTIONALLY_ENCLOSED_BY=NONE,
+    REPLACE_INVALID_CHARACTERS=TRUE,
+    DATE_FORMAT=AUTO,
+    TIME_FORMAT=AUTO,
+    TIMESTAMP_FORMAT=AUTO,
+    EMPTY_FIELD_AS_NULL = FALSE,
+    ERROR_ON_COLUMN_COUNT_MISMATCH=FALSE
+)
+ON_ERROR=CONTINUE
+FORCE = TRUE;
+
+COPY INTO cortex_analyst_demo.healthcare.patients
+FROM @raw_data
+FILES = ('patients.csv.gz')
+FILE_FORMAT = (
+    TYPE=CSV,
+    COMPRESSION=GZIP,
+    SKIP_HEADER=1,
+    FIELD_DELIMITER=',',
+    TRIM_SPACE=FALSE,
+    FIELD_OPTIONALLY_ENCLOSED_BY=NONE,
+    REPLACE_INVALID_CHARACTERS=TRUE,
+    DATE_FORMAT=AUTO,
+    TIME_FORMAT=AUTO,
+    TIMESTAMP_FORMAT=AUTO,
+    EMPTY_FIELD_AS_NULL = FALSE,
+    ERROR_ON_COLUMN_COUNT_MISMATCH=FALSE
+)
+ON_ERROR=CONTINUE
+FORCE = TRUE;
+
+COPY INTO cortex_analyst_demo.healthcare.providers
+FROM @raw_data
+FILES = ('providers.csv.gz')
+FILE_FORMAT = (
+    TYPE=CSV,
+    COMPRESSION=GZIP,
+    SKIP_HEADER=1,
+    FIELD_DELIMITER=',',
+    TRIM_SPACE=FALSE,
+    FIELD_OPTIONALLY_ENCLOSED_BY=NONE,
+    REPLACE_INVALID_CHARACTERS=TRUE,
+    DATE_FORMAT=AUTO,
+    TIME_FORMAT=AUTO,
+    TIMESTAMP_FORMAT=AUTO,
+    EMPTY_FIELD_AS_NULL = FALSE,
+    ERROR_ON_COLUMN_COUNT_MISMATCH=FALSE
+)
+ON_ERROR=CONTINUE
+FORCE = TRUE;
+
+COPY INTO cortex_analyst_demo.healthcare.schedule_templates
+FROM @raw_data
+FILES = ('schedule_templates.csv.gz')
+FILE_FORMAT = (
+    TYPE=CSV,
+    COMPRESSION=GZIP,
+    SKIP_HEADER=1,
+    FIELD_DELIMITER=',',
+    TRIM_SPACE=FALSE,
+    FIELD_OPTIONALLY_ENCLOSED_BY=NONE,
+    REPLACE_INVALID_CHARACTERS=TRUE,
+    DATE_FORMAT=AUTO,
+    TIME_FORMAT=AUTO,
+    TIMESTAMP_FORMAT=AUTO,
+    EMPTY_FIELD_AS_NULL = FALSE,
+    ERROR_ON_COLUMN_COUNT_MISMATCH=FALSE
+)
+ON_ERROR=CONTINUE
+FORCE = TRUE;
+
+-- Upload appointments.csv
+-- Upload appointments.csv
+PUT file://C:/Users/SaiKumarReddyKatika/Downloads/sfguide-getting-started-with-cortex-analyst-main/sfguide-getting-started-with-cortex-analyst-main/appointments.csv @raw_data;
+
+-- Upload patients.csv
+PUT file://C:/Users/SaiKumarReddyKatika/Downloads/sfguide-getting-started-with-cortex-analyst-main/sfguide-getting-started-with-cortex-analyst-main/patients.csv @raw_data;
+
+-- Upload providers.csv
+PUT file://C:/Users/SaiKumarReddyKatika/Downloads/sfguide-getting-started-with-cortex-analyst-main/sfguide-getting-started-with-cortex-analyst-main/providers.csv @raw_data;
+
+-- Upload schedule_templates.csv
+PUT file://C:/Users/SaiKumarReddyKatika/Downloads/sfguide-getting-started-with-cortex-analyst-main/sfguide-getting-started-with-cortex-analyst-main/schedule_templates.csv @raw_data;
+
+LIST @raw_data;
